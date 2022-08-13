10d3ea561815f3f3bdb6c99be0fa2fc08393ae2c
KenguKn <111166867+KenguKn@users.noreply.github.com>
2022-08-13 11:41:16 +0700

    Add files via upload

---
+++ งาน.py
@@ -0,0 +1,15 @@
+school="dsru"
+classroom="3/9"
+studentid="63276"
+number="7"
+mobile="0924852909"
+print("School:",school,"                            ","Classroom:",classroom)
+print("Student ID:",studentid,"                       ","number:",number)
+print("Mobile:",mobile)
+
+print("TTTTTTTTT","     ","OOOOOOOOO","     ","NN      NN","     ","GGGGGGGGGG","     ","   JJJJJJJ")
+print("   TTT   ","     ","OO     OO","     ","NNNN    NN","     ","GG        ","     ","        JJ")
+print("   TTT   ","     ","OO     OO","     ","NN  NN  NN","     ","GG    GGGG","     ","        JJ")
+print("   TTT   ","     ","OO     OO","     ","NN   NN NN","     ","GG      GG","     ","        JJ")
+print("   TTT   ","     ","OO     OO","     ","NN    NNNN","     ","GG      GG","     ","J       JJ")
+print("   TTT   ","     ","OOOOOOOOO","     ","NN      NN","     "," GGGGGGGG ","     "," JJJJJJJJ")
